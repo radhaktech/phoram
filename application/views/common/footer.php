67d0da5336ac2550f2dca6d33842bc05cb972ef2
Srinivasa Prasad <prasad.srinivas82@gmail.com>
2015-03-18 18:36:25 +0530

    Initial Commit

---
+++ application/views/common/footer.php
@@ -0,0 +1,9 @@
+</div> <!-- /container -->
+
+<!-- Bootstrap core Javascript ---->
+
+<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="<?php echo base_url('bootstrap/js/bootstrap.min.js');?>"></script>
+<script src="<?php echo base_url('bootstrap/js/docs.min.js');?>"></script>
+</body>
+</html>
